3db8db55f552e878c8fa7a228f360444e5330384
unknown <shani0512@gmail.com>
2019-01-26 17:05:47 +0200

    added firebase authenticaion (sign in)

--- app/src/main/java/com/example/demo/appdemo/MainActivity.java
+++ app/src/main/java/com/example/demo/appdemo/MainActivity.java
@@ -1,42 +1,69 @@
 package com.example.demo.appdemo;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+// Firebase authentication related imports
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.AuthResult;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = ".MainActivity";
+
+    private FirebaseAuth mAuth;
+
+    private EditText emailField, passwordField;
+    private Button loginButton, signupButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button loginButton = findViewById(R.id.login_btn);
-        loginButton.setOnClickListener(new View.OnClickListener() {
 
-            // login button onClick
+        // UI components
+        loginButton = findViewById(R.id.login_btn);
+        signupButton = findViewById(R.id.signup_btn);
+        emailField = findViewById(R.id.email);
+        passwordField = findViewById(R.id.password);
+
+        // firebase authentication
+        mAuth = FirebaseAuth.getInstance();
+
+        // login button onClick
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                String email = emailField.getText().toString();
+                String pass = passwordField.getText().toString();
 
-                // ToDo: verify Login username and password
-                /*
-                // message to next activity
-                EditText editText = (EditText) findViewById(R.id.editText);
-                String message = editText.getText().toString();
-                intent.putExtra(EXTRA_MESSAGE, message);*/
-
-                startActivity(intent);
-
+                //TODO: add validation function
+                //TODO: add regular expression for email address and password
+                if(!email.equals("") && !pass.equals(""))
+                {
+                    signIn(email,pass);
+                }
+                else
+                {
+                    toastMessage("Please fill in all the fields.");
+                }
             }
         });
 
-
-        Button signupButton = findViewById(R.id.signup_btn);
+        // sign up button on click
         signupButton.setOnClickListener(new View.OnClickListener() {
-
-            // login button onClick
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
@@ -44,5 +71,44 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+
+    /**
+     * signIn function receives an email address and a password and authenticates the user
+     * if sign in succeeded, it start HomeActivity
+     * else, it displays a message
+     *
+     * @param email the typed email
+     * @param password the typed password
+     */
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in succeeded, start HomeActivity
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            // If sign in failed, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            toastMessage("Authentication failed.");
+                        }
+                    }
+                });
+    }
+
+
+    /**
+     * toastMessage function receives a message and displays
+     * @param message the message to show
+     */
+    private void toastMessage(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 }
