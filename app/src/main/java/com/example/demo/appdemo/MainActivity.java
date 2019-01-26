0a74a5a04146649c53be792d7e11edb4274b2c96
unknown <shani0512@gmail.com>
2019-01-26 17:52:39 +0200

    added firebase authentication (sign up)

--- app/src/main/java/com/example/demo/appdemo/MainActivity.java
+++ app/src/main/java/com/example/demo/appdemo/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.demo.appdemo;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.util.Log;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -72,6 +73,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    //TODO: check if user is already signed in (onStart())
 
 
     /**
